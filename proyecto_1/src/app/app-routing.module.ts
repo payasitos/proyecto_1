971a5d10da3ede8180e375d1a99a5bd5ed92d436
payasitos <elgalaxyboii@gmail.com>
2024-04-22 08:42:57 -0300

    cambios

--- proyecto_1/src/app/app-routing.module.ts
+++ proyecto_1/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './secciones/inicio/inicio.component';
+import { GaleriaComponent } from './secciones/galeria/galeria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inicio", component: InicioComponent
+  },
+  {
+    path: "galeria", component: GaleriaComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto_1/src/app/app.module.ts
+++ proyecto_1/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './secciones/inicio/inicio.component';
+import { GaleriaComponent } from './secciones/galeria/galeria.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RootNavComponent } from './root-nav/root-nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    GaleriaComponent,
+    RootNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
